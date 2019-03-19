6f061b1d6765153132b56cc249ee14a459238c4d
Luis <luis.romero14@upr.edu>
2019-03-18 20:59:05 -0500

    web form added

---
+++ server.js
@@ -0,0 +1,77 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+const express = require("express");
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const PORT = process.env.PORT || 3000;
+
+httpServer.listen(3000, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+// put the HTML file containing your form in a directory named "public" (relative to where this script is located)
+app.get("/", express.static(path.join(__dirname, "./public")));
+
+const multer = require("multer");
+
+const handleError = (err, res) => {
+  res
+    .status(500)
+    .contentType("text/plain")
+    .end("Oops! Something went wrong!");
+};
+
+const upload = multer({
+  dest: "/path/to/temporary/directory/to/store/uploaded/files"
+  // you might also want to set some limits: https://github.com/expressjs/multer#limits
+});
+
+
+app.post(
+  "/upload",
+  upload.single("file" /* name attribute of <file> element in your form */),
+  (req, res) => {
+    const tempPath = req.file.path;
+    let num = req.body.stdNumber;
+    const targetPath = path.join(__dirname, "./uploads/"+num+".jpg");
+    console.log(req);
+
+    fs.readFile('db.json', function (err, data) {
+    	var json = JSON.parse(data)
+    	json.push({
+	nombre:req.body.stdName,
+    	sexo:req.body.optionMF,
+    	studentNum:req.body.stdNumber,
+    	history:[],
+    	hasAccess:req.body.hasAccess === 'on'?true:false,
+	image:targetPath
+	})
+
+    	fs.writeFile("db.json", JSON.stringify(json))
+    })
+
+    if (path.extname(req.file.originalname).toLowerCase() === ".jpg"||req.file.originalname).toLowerCase() === ".png"||req.file.originalname).toLowerCase() === ".jpeg") {
+      fs.rename(tempPath, targetPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(200)
+          .contentType("text/plain")
+          .end("File uploaded!");
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) return handleError(err, res);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .jpg files are allowed!");
+      });
+    }
+  }
+);
